662b582c76082a9f70911f7c476153b35ed09672
Alan Fernandez <alan.f@msn.com>
2023-06-03 19:30:31 +0100

    logging

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as morgan from 'morgan';
 import { Request, Response } from 'express';
 import { AppDataSource } from './data-source';
 import { Routes } from './routes';
 import { User } from './entity/User';
 
-import { port } from "./config";
+import { morganConfig, port } from './config';
 
 function handleError(error, req, res, next) {
   res.status(error.statusCode || 500).send({ message: error.message });
@@ -14,6 +15,7 @@ function handleError(error, req, res, next) {
 AppDataSource.initialize()
   .then(async () => {
     const app = express();
+    app.use(morgan(morganConfig));
     app.use(bodyParser.json());
 
     Routes.forEach((route) => {
