3a387567003e0470af6855dc29d21229dd1de417
Alan Fernandez <alan.f@msn.com>
2023-06-11 17:31:34 +0100

    users test refactor

--- __tests__/acceptance/users.test.ts
+++ __tests__/acceptance/users.test.ts
@@ -11,40 +11,48 @@ const testUser = {
   age: 33,
 };
 
-beforeEach(async () => {
-  connection = await AppDataSource.initialize();
-  await connection.synchronize(true);
-  server = app.listen(port);
-});
+describe('User API tests', () => {
+  beforeEach(async () => {
+    connection = await AppDataSource.initialize();
+    await connection.synchronize(true);
+    server = app.listen(port);
+  });
 
-afterEach(async () => {
-  connection.close();
-  server.close();
-});
+  afterEach(async () => {
+    try {
+      await connection.close();
+      await server.close();
+    } catch (error) {
+      console.error('Error closing resources:', error);
+    }
+  });
 
-it('should be no users initially', async () => {
-  const response = await request(app).get('/users');
-  expect(response.statusCode).toBe(200);
-  expect(response.body).toEqual([]);
-});
+  it('should return an empty array when there are no users', async () => {
+    const response = await request(app).get('/users');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual([]);
+  });
 
-it('should create a user', async () => {
-  const response = await request(app).post('/users').send(testUser);
-  expect(response.statusCode).toBe(200);
-  expect(response.body).toEqual({ ...testUser, id: 1 });
-});
+  it('should successfully create a user and return the created user', async () => {
+    const response = await request(app).post('/users').send(testUser);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toMatchObject(testUser);
+  });
 
-it('should not create a user if no firstName is given', async () => {
-  const response = await request(app)
-    .post('/users')
-    .send({ lastName: 'Doe', age: 33 });
-  expect(response.statusCode).toBe(400);
-  expect(response.body.errors).not.toBeNull();
-  expect(response.body.errors.length).toBe(1);
-  expect(response.body.errors[0]).toEqual({
-    location: 'body',
-    msg: 'Invalid value',
-    path: 'firstName',
-    type: 'field',
+  it('should return a 400 status and an error when creating a user without a firstName', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({ lastName: 'Doe', age: 33 });
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toHaveProperty('errors');
+    expect(response.body.errors).toBeInstanceOf(Array);
+    expect(response.body.errors).toHaveLength(1);
+    expect(response.body.errors[0]).toMatchObject({
+      location: 'body',
+      msg: 'Invalid value',
+      path: 'firstName',
+      type: 'field',
+    });
   });
 });
