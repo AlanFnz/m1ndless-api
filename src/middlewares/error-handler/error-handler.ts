42419e8a11a78a9b73552ded2c8017292673e30e
Alan Fernandez <alan.f@msn.com>
2023-06-19 20:39:12 +0100

    extended error handler

--- src/middlewares/error-handler/error-handler.ts
+++ src/middlewares/error-handler/error-handler.ts
@@ -1,6 +1,8 @@
 import bunyan from 'bunyan';
-import { logger } from '../logger';
+import { NextFunction, Request, Response } from 'express';
+import { APIError } from './api-error';
 import { BaseError } from './base-error';
+import { logger } from '../logger';
 
 class ErrorHandler {
   logger: bunyan;
@@ -9,11 +11,30 @@ class ErrorHandler {
     this.logger = logger;
   }
 
-  public async handleError(err: Error): Promise<void> {
-    logger.error(err);
+  public async handleError(
+    error: Error,
+    req?: Request,
+    res?: Response,
+    next?: NextFunction
+  ): Promise<void> {
+    if (this.isTrustedError(error)) {
+      const apiError = error as APIError;
+      this.logger.error(apiError);
+
+      res.status(apiError.httpCode || 500).json({
+        message: apiError.message,
+        endpoint: apiError.methodName,
+      });
+    } else {
+      this.logger.error(error);
+
+      res.status(500).json({
+        message: 'An unexpected error occurred.',
+      });
+    }
   }
 
-  public isTrustedError(error: Error) {
+  public isTrustedError(error: Error): boolean {
     return error instanceof BaseError && error.isOperational;
   }
 }
--- src/middlewares/error-handler/index.ts
+++ src/middlewares/error-handler/index.ts
@@ -1,15 +1,15 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { errorHandler } from './error-handler';
 
 export async function handleError(
   error: any,
   req: Request,
   res: Response,
-  next: Function
+  next: NextFunction
 ): Promise<void> {
   if (!errorHandler.isTrustedError(error)) {
     next(error);
     return;
   }
-  await errorHandler.handleError(error);
+  await errorHandler.handleError(error, req, res, next);
 }
